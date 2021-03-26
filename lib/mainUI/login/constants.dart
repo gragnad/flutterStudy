07c7883b0108a7b0fd461cbadafe3a4153f2467f
gragnad <gragnad88@gmail.com>
2021-03-26 17:59:25 +0900

    study 2

---
+++ lib/mainUI/login/constants.dart
@@ -0,0 +1,5 @@
+//https://github.com/NearHuscarl/flutter_login/blob/master/example/lib/constants.dart
+class Constants {
+  static const String appName = 'TraditionalDelivery';
+  static const String logoTag = '';
+}
